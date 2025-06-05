b7cc4238fd9fec8c2df411a9a7c5234356118d67
Cwolfe13 <wolfe.connor@gmail.com>
2025-06-05 18:13:11 -0400

    Setup pagination controls and pagination logic.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -16,13 +16,25 @@ import { useDebounce, useJobItems } from "../hooks/hooks";
 import { Toaster } from "react-hot-toast";
 
 function App() {
+  //state
   const [searchText, setSearchText] = useState("");
   const debouncedSearchText = useDebounce(searchText, 250);
-
   const { jobItems, isLoading } = useJobItems(debouncedSearchText);
+  const [currentPage, setCurrentPage] = useState(1);
 
+  // derived state
   const totalNumberOfResults = jobItems?.length || 0;
-  const jobItemsSliced = jobItems?.slice(0, 7) || []; // Limit to 10 items for pagination
+  const jobItemsSliced =
+    jobItems?.slice(currentPage * 7 - 7, currentPage * 7) || []; // Limit to 10 items for pagination
+
+  // state change functions
+  const handleChangePage = (direction: "next" | "previous") => {
+    if (direction === "next") {
+      setCurrentPage((prev) => prev + 1);
+    } else if (direction === "previous") {
+      setCurrentPage((prev) => Math.max(prev - 1, 1));
+    }
+  };
 
   return (
     <>
@@ -44,7 +56,10 @@ function App() {
             <SortingControls />
           </SidebarTop>
           <JobList jobItems={jobItemsSliced} isLoading={isLoading} />
-          <PaginationControls />
+          <PaginationControls
+            currentPage={currentPage}
+            onClick={handleChangePage}
+          />
         </Sidebar>
         <JobItemContent />
       </Container>
--- src/components/PaginationControls.tsx
+++ src/components/PaginationControls.tsx
@@ -1,3 +1,59 @@
-export default function PaginationControls() {
-  return <section className="pagination"></section>;
+import { ArrowLeftIcon, ArrowRightIcon } from "@radix-ui/react-icons";
+
+type PaginationControlsProps = {
+  currentPage: number;
+  onClick: (direction: "next" | "previous") => void;
+};
+
+export default function PaginationControls({
+  currentPage,
+  onClick,
+}: PaginationControlsProps) {
+  return (
+    <section className="pagination">
+      {currentPage > 1 && (
+        <PaginationButton
+          direction={"previous"}
+          currentPage={currentPage}
+          onClick={() => onClick("previous")}
+        />
+      )}
+
+      <PaginationButton
+        direction={"next"}
+        currentPage={currentPage}
+        onClick={() => onClick("next")}
+      />
+    </section>
+  );
+}
+
+function PaginationButton({
+  direction,
+  currentPage,
+  onClick,
+}: {
+  direction: "next" | "previous";
+  currentPage: number;
+  onClick: () => void;
+}) {
+  return (
+    <button
+      className={`pagination__button pagination__button--${direction}`}
+      onClick={onClick}
+    >
+      {direction === "previous" && (
+        <>
+          <ArrowLeftIcon />
+          Page {currentPage - 1}
+        </>
+      )}{" "}
+      {direction === "next" && (
+        <>
+          <ArrowRightIcon />
+          Page {currentPage + 1}
+        </>
+      )}
+    </button>
+  );
 }
