0ca049d05314ca5b2c9266e441c234e7752fe311
Cwolfe13 <wolfe.connor@gmail.com>
2025-06-05 11:22:52 -0400

    Implement custom hook to grab the active jobItem.

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,5 +1,43 @@
 import { useEffect, useState } from "react";
 import { JobItem } from "../lib/types";
+import { BASE_URL } from "../lib/constants";
+
+export function useActiveId() {
+  const [activeId, setActiveId] = useState<number | null>(null);
+
+  useEffect(() => {
+    const handleHashChange = () => {
+      // + converts the string to a number;
+      const id = +window.location.hash.slice(1);
+      setActiveId(id);
+    };
+    handleHashChange();
+    window.addEventListener("hashchange", handleHashChange);
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener("hashchange", handleHashChange);
+    };
+  }, []);
+
+  return activeId;
+}
+
+export function useJobItem(id: number | null) {
+  const [jobItem, setJobItem] = useState(null);
+
+  useEffect(() => {
+    if (!id) return;
+
+    const fetchData = async () => {
+      const response = await fetch(`${BASE_URL}/${id}`);
+      const data = await response.json();
+      setJobItem(data.jobItem);
+    };
+    fetchData();
+  }, [id]);
+
+  return jobItem;
+}
 
 export function useJobItems(searchText: string) {
   const [jobItems, setJobItems] = useState<JobItem[]>([]);
@@ -11,9 +49,7 @@ export function useJobItems(searchText: string) {
     if (!searchText) return;
     setIsLoading(true);
     const fetchData = async () => {
-      const response = await fetch(
-        `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`
-      );
+      const response = await fetch(`${BASE_URL}?search=${searchText}`);
       const data = await response.json();
       setIsLoading(false);
       setJobItems(data.jobItems);
